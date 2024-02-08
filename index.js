e71c25056712368d1b65210881919f832c625fa0
Vishal Maharathy <vishalmaharathy76@gmail.com>
2024-02-08 16:22:39 +0530

    typescript init

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 "use strict";
-function sum(a, b) {
-    return a + b;
+function fun1(a) {
+    console.log(a ^ 100);
 }
-console.log(sum(1, 2));
+let interval = 1000;
+function runAnotherFunction(fn, interval) {
+    setTimeout(fn, interval);
+}
+runAnotherFunction(fun1, interval);
--- index.ts
+++ index.ts
@@ -1,5 +1,9 @@
-function sum(a: number, b: number): number {
-  return a + b
-}
 
-console.log(sum(1, 2))
+function fun1(a:number){
+  console.log(a^100)
+}
+let interval:number=1000
+function runAnotherFunction(fn:(a:number)=>void, interval:number){
+  setTimeout(fn, interval)
+}
+runAnotherFunction(fun1, interval)
